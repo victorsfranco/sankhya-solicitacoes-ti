00a7df47edb7abaf3697d1c140c840298870203f
victorsfranco <victorsfranco@outlook.com>
2022-09-11 16:20:36 -0300

    Implementação do botão Ocultar Detalhes da OS.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function incluirRetorno() {
   document.getElementById('retorno').innerHTML = `
     <div id="incluirRetorno">
       <div>
-        <label for="encerrarOs">Encerrar O.S:</label>
-        <select name="encerrarOs" id="encerra">
+        <label for="encerrarOS">Encerrar O.S:</label>
+        <select name="encerrarOS" id="encerra">
           <option value="1">Sim</option>
           <option value="2">Não</option>
         </select>
@@ -22,23 +22,54 @@ function enviar() {
 
   switch (encerraValor) {
     case '1':
-      var statusOs = 'Concluída'
+      var statusOS = 'Concluída'
       break
     case '2':
-      var statusOs = 'Aguardando atendimento'
+      var statusOS = 'Aguardando atendimento'
   }
 
   document.getElementById('processamentos').innerHTML = `
   <div class="cabecario">
    <h3>Atividades Recentes</h3>
-    <button onclick="incluirRetorno()">Incluir Retorno</button>
+    <button onclick="incluirRetorno()" id="botaoIncluirRetorno">Incluir Retorno</button>
+    <button onclick="ocultarProcessamentosOS()" class="alternaVisualizacao" id="botaoOcultaProcessamentos">-</button>
   </div>
   <ul id="respostaProcessamento">
     <li id="autor">MYKAELSON.SOUSA</li>
-    <li id="status">${statusOs} - 03/08/2022 17:33:47</li>
+    <li id="status">${statusOS} - 03/08/2022 17:33:47</li>
     <li>
       ${mensagem}
     </li>
   </ul>
   `
+}
+
+var detalhesOS = document.getElementById('listaDetalhesOS')
+var detalhesVisiveis = true
+
+function ocultarDetalhesOS() {
+
+
+  if (detalhesVisiveis == true) {
+    detalhesOS.style.display = "none"
+    detalhesVisiveis = false
+  } else if (detalhesVisiveis == false) {
+    detalhesOS.style.display = "block"
+    detalhesVisiveis = true
+  } else {}
+
+}
+
+var processamentosOS = document.getElementById('respostaProcessamento')
+var processamentosVisiveis = true
+
+function ocultarProcessamentosOS() {
+  console.log(processamentosOS);
+  if (processamentosVisiveis == true) {
+    processamentosOS.style.display = "none"
+    processamentosVisiveis = false
+  } else if (processamentosVisiveis == false) {
+    processamentosOS.style.display = "block"
+    processamentosVisiveis = true
+  } else {}
 }
