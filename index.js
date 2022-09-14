2152acb14c2c6c7b2794311e0e40b810e3bc1057
Victor Antônio Silva Franco <victor.franco@GRUPOKOTHE.LOCAL>
2022-09-14 18:07:58 -0300

    Preparação para importação de campos do Saknhya

--- index.js
+++ index.js
@@ -11,27 +11,31 @@ var horaAtual = new Intl.DateTimeFormat('pt-BR', {
 
 var detalhesOS = document.getElementById('listaDetalhesOS')
 var detalhesVisiveis = true
-
+document.getElementById('botaoOcultaDetalhesOS').innerHTML = '-'
 function ocultarDetalhesOS() {
   if (detalhesVisiveis == true) {
     detalhesOS.style.display = 'none'
+    document.getElementById('botaoOcultaDetalhesOS').innerHTML = '+'
     detalhesVisiveis = false
   } else if (detalhesVisiveis == false) {
     detalhesOS.style.display = 'block'
+    document.getElementById('botaoOcultaDetalhesOS').innerHTML = '-'
     detalhesVisiveis = true
   }
 }
 
 var processamentosOS = document.getElementById('Processamentos')
-var processamentosVisiveis = true
+var processamentosVisiveis = false
+document.getElementById('botaoOcultaProcessamentos').innerHTML = '-'
 
 function ocultarProcessamentosOS() {
-  console.log(processamentosOS)
   if (processamentosVisiveis == true) {
     processamentosOS.style.display = 'none'
+    document.getElementById('botaoOcultaProcessamentos').innerHTML = '+'
     processamentosVisiveis = false
   } else if (processamentosVisiveis == false) {
     processamentosOS.style.display = 'block'
+    document.getElementById('botaoOcultaProcessamentos').innerHTML = '-'
     processamentosVisiveis = true
   }
 }
@@ -49,6 +53,39 @@ function incluirRetorno() {
   }
 }
 
+//Importação de campos - SANKHYA
+// Número Único da OS:
+document.getElementById('numeroUnicoOS').innerHTML = '#34367'
+
+// Título da OS:
+document.getElementById('tituloOS').innerHTML =
+  'Erro ao alterar tipo de Contrato'
+
+// Solicitante:
+document.getElementById('solicitante').innerHTML = 'VICTOR ANTONIO SILVA FRANCO'
+
+//Departamento:
+document.getElementById('departamento').innerHTML =
+  'TI - TECNOLOGIA INFORMAÇÃO-AUX'
+
+//Sistema:
+document.getElementById('sistemaOS').innerHTML = 'Sankhya - Kothe'
+
+//Status:
+document.getElementById('statusOS').innerHTML = 'Encerrada'
+
+//Descrição da O.S:
+document.getElementById(
+  'descricaoOS'
+).innerHTML = `Boa tarde. Na tela Contratos de T.I , estou obtendo o erro
+apresentado na foto em anexo ao tentar alterar o TIPO DE CONTRATO
+do lançamento nº 1 . Desejo alterar o TIPO DE CONTRATO de
+"INTERNET" para "PRESTAÇÃO DE SERVIÇOS", e não estou conseguindo
+devido a esse erro. Obs: O erro acontece tambem em outros
+lançamentos.`
+
+//Inclusão do Processamento da O.S:
+
 var mensagens = []
 
 function enviar() {
@@ -64,14 +101,14 @@ function enviar() {
   mensagens.push(
     `
     <ul id="teste">
-      <li id="autor">MYKAELSON.SOUSA</li>
+      <li id="autor"></li>
       <li id="status">${statusOS} - ${dataAtual} ${horaAtual}</li>
       <li>${document.getElementById('retorno').value}</li>
     </ul>
     `
   )
 
-  var listaProcessamentos = mensagens.join(' ')
+  var listaProcessamentos = mensagens.join('')
 
   document.getElementById('processamentosExistentes').innerHTML =
     listaProcessamentos
