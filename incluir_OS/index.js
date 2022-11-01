26ac3225d6f9dd38151bbd2e2172700b98a19754
Victor Antônio Silva Franco <victor.franco@GRUPOKOTHE.LOCAL>
2022-11-01 17:56:58 -0300

    feat: creating saveRequest function

--- incluir_OS/index.js
+++ incluir_OS/index.js
@@ -1,11 +1,37 @@
-const requestTitle = document.querySelector('#request-title').value;
+let requestTitle = document.querySelector('#request-title').value;
 
-const requestSystem = document.querySelector('#request-system').value;
+document.querySelector('#request-title').addEventListener('change', (event) => {
+   requestTitle = event.target.value;
+})
 
-const problemLocation = document.querySelector('#problem-location').value;
+let requestSystem = document.querySelector('#request-system').value;
 
-const descriptionText = document.querySelector('#description-text').value;
+document.querySelector('#request-system').addEventListener('change', (event) => {
+   requestSystem = event.target.value;
+})
 
+let problemLocation = document.querySelector('#problem-location').value;
+
+document.querySelector('#problem-location').addEventListener('change', (event) => {
+   problemLocation = event.target.value
+})
+
+let descriptionText = document.querySelector('#description-text').value;
+
+document.querySelector('#description-text').addEventListener('change', (event) => {
+   descriptionText = event.target.value
+})
+
+function saveRequest() {
+   const requestData = {
+      title: requestTitle,
+      system: requestSystem,
+      location: problemLocation,
+      description: descriptionText,
+
+   }
+   console.log(requestData);
+}
 
 function cancelRequest() {
    console.log('Cancelou');
